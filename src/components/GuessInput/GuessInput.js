ac95245fad5fc20fec8a4e69e671db10703df34c
LindaKat <lindakt16@gmail.com>
2023-04-18 20:31:17 -0500

    exercise 1 solved

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 function GuessInput() {
+
+  const [guess, setGuess] = React.useState("");
+
   return (
-    <form class="guess-input-wrapper">
-      <label for="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.info({ guess });
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => {
+          setGuess(e.target.value.toUpperCase());
+        }}
+        pattern="([A-Z]){5}"
+        title="Guess must be 5 letters long"
+      />
+      <span className="guess-input-hint">Guess a 5 letter word</span>
     </form>
   );
 }
