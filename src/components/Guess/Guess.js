d4256ad23c76f89298074a57b7a6051380e57608
LindaKat <lindakt16@gmail.com>
2023-04-21 21:28:09 -0500

    exercise 4 solved

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { range } from "../../utils";
 import { WORD_LENGTH } from "../../constants";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const results = checkGuess(value, answer);
+
   return (
     <p className="guess">
       {range(WORD_LENGTH).map((num) => (
-        <span className="cell" key={num}>
+        <span className={`cell ${value && results[num].status}`} key={num}>
           {value ? value[num] : undefined}
         </span>
       ))}
