5dd57ce22527706242ccb8b7547fca6c02364ca5
LindaKat <lindakt16@gmail.com>
2023-04-21 22:19:42 -0500

    exercise 4 solution feedback

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,15 +3,23 @@ import { range } from "../../utils";
 import { WORD_LENGTH } from "../../constants";
 import { checkGuess } from "../../game-helpers";
 
+// hadn't thought of this - having another "component" within the same file
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value, answer }) {
-  const results = checkGuess(value, answer);
+  const result = checkGuess(value, answer);
 
   return (
     <p className="guess">
       {range(WORD_LENGTH).map((num) => (
-        <span className={`cell ${value && results[num].status}`} key={num}>
-          {value ? value[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
